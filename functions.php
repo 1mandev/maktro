e15f62eacd385dddd2e81ce67a0c71579496c0d9
Shahriar Parvez <mr.shahriyarshuvo@gmail.com>
2019-12-09 08:09:08 +0600

    Update functions.php

--- functions.php
+++ functions.php
@@ -14,29 +14,11 @@ function maktro_enqueue_styles() {
     wp_get_theme()->get('Version')
 );
 
-wp_enqueue_script( 'maktro-jquery', '//code.jquery.com/jquery-3.4.1.min.js', array(), wp_get_theme()->get('Version'), true );
+    wp_enqueue_script( 'maktro-jquery', '//code.jquery.com/jquery-3.4.1.min.js', array(), wp_get_theme()->get('Version'), true );
     wp_enqueue_script( 'maktro-foundation', '//cdn.jsdelivr.net/npm/foundation-sites@6.5.3/dist/js/foundation.min.js', array('maktro-jquery'), wp_get_theme()->get('Version'), true );
     wp_enqueue_script( 'maktro-scripts', get_stylesheet_directory_uri() . '/maktro-scripts.js', array('maktro-jquery', 'maktro-foundation'), wp_get_theme()->get('Version'), true );
 
     wp_enqueue_script( 'maktro-fontawesome', '//kit.fontawesome.com/554d3e6589.js', array(), wp_get_theme()->get('Version'), true );
     
     
-}
-
-
-function bootstrap_load(){
-   
-
-}
-function register_bootstrap() {
-	add_action( 'wp_enqueue_scripts',  'boostrap_load');
-}
-
-
-function bootstrap_unload(){
-   wp_deregister_style( 'maktro-bootstrap' );
-}
-
-function deregister_bootstrap() {
-	add_action( 'wp_enqueue_scripts',  'boostrap_unload');
 }
